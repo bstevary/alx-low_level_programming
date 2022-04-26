fcbdaf32fd51e5b7a133b2bdfed5aed222287cae
bstevary <bstevary@gmail.com>
2022-04-26 13:12:01 +0300

    late project initialization

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * before_main - prints statement before running main func
+ */
+void __attribute__((constructor)) before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
